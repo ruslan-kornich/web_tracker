9781aecd29b5ba4c7fe080fbf698e87439512f66
Ruslan Kornich <ruslan.kornich@ronis.info>
2024-05-19 10:25:40 +0300

    admin register models

--- backend/analytics/admin.py
+++ backend/analytics/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Campaign, Offer, Click, Lead
 
-# Register your models here.
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date', 'budget', 'created_at', 'updated_at')
+    search_fields = ('name', 'description')
+    list_filter = ('start_date', 'end_date')
+
+
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('name', 'campaign', 'url', 'created_at', 'updated_at')
+    search_fields = ('name', 'description')
+    list_filter = ('campaign',)
+
+
+@admin.register(Click)
+class ClickAdmin(admin.ModelAdmin):
+    list_display = (
+    'offer', 'user_ip', 'user_agent', 'os', 'browser', 'click_time', 'landing_page_url', 'created_at', 'updated_at')
+    search_fields = ('user_ip', 'user_agent', 'os', 'browser')
+    list_filter = ('offer', 'click_time')
+
+
+@admin.register(Lead)
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ('click', 'lead_time', 'created_at', 'updated_at')
+    list_filter = ('click', 'lead_time')
