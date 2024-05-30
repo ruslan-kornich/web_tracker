0c7fb28921eb55e6a8c09f0c5a5b7d6e222eddc3
Ruslan Kornich <ruslan.kornich@ronis.info>
2024-05-30 18:03:38 +0300

    data + docker

--- backend/web_tracker/settings.py
+++ backend/web_tracker/settings.py
@@ -83,13 +83,24 @@ WSGI_APPLICATION = "web_tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tracker',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres12',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
